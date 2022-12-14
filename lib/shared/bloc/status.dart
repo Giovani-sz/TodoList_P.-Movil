54921ee93be589ebd9213c972fc1c0e349ee780c
giovani <giovaniszg@gmail.com>
2022-12-14 17:10:06 -0500

    agregado de la lista y demas

---
+++ lib/shared/bloc/status.dart
@@ -0,0 +1,18 @@
+abstract class appstatus {}
+
+class appinitialsyatus extends appstatus {}
+
+class appchange_navbarstatus extends appstatus {}
+
+// La clase Appinitialsyatus extiende el estado de AppStatus {}
+class Appcreatdatabasestate extends appstatus {}
+
+class Appinsertdatabasestate extends appstatus {}
+
+class Appgetdatabasestate extends appstatus {}
+
+class Appapdetdatabasestate extends appstatus {}
+
+class AppDeletdatabasestate extends appstatus {}
+
+class Appchangiconstate extends appstatus {}
